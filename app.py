802c1579b3278b4a1f541b66334aae8c1ac48f6f
Alexandru Incicau <aincicau@yahoo.com>
2022-01-10 16:39:31 +0200

    Moved loading moved out of endpoint

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from PIL import Image
 import numpy as np
 from skimage import transform
 
+
+model = tf.keras.models.load_model('./model/my_model')
+
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
@@ -11,7 +14,6 @@ def hello_world():
 
 @app.route('/process')
 def process():
-    model = tf.keras.models.load_model('./model/my_model')
     image = Image.open('./input/00001.jpg')
     image = np.array(image).astype('float32')/255
     image = transform.resize(image, (120, 120, 3))
