6d4663afffe320d76ba6f95a182902e62a1f55a5
Alexandru Incicau <aincicau@yahoo.com>
2022-01-10 16:34:36 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+from skimage import transform
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/process')
+def process():
+    model = tf.keras.models.load_model('./model/my_model')
+    image = Image.open('./input/00001.jpg')
+    image = np.array(image).astype('float32')/119
+    image = transform.resize(image, (120, 120, 3))
+    image = np.expand_dims(image, axis=0)
+
+    print(model.predict(image))
+
+    return 'Processed'
