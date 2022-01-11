ce85406e03817a0047ca51e09865f1cda595c06d
Alexandru Incicau <aincicau@yahoo.com>
2022-01-11 22:35:47 +0200

    Handle uploaded file, conversion for inference tbd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, Response
+from json import dumps
 import tensorflow as tf
 from PIL import Image
 import numpy as np
@@ -8,18 +9,29 @@ from skimage import transform
 model = tf.keras.models.load_model('./model/my_model')
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
-@app.route('/process')
+@app.route('/process', methods=['POST'])
 def process():
     # imagefile = flask.request.files.get('imagefile', '')
+    # if 'file' in request.files:
+    #     print('yes')
+    print(request.files)
+
     image = Image.open('./input/00001.jpg')
+    #image = request.files['file']
     image = np.array(image).astype('float32')/255
     image = transform.resize(image, (120, 120, 3))
     image = np.expand_dims(image, axis=0)
 
-    print(model.predict(image))
+    result = model.predict(image)
+    print(result)
 
-    return 'Processed'
+    resp = Response('casd')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+
+    return resp
