703892e2b5aea5635ddebb3fe966b08a746b244a
Alexandru Incicau <aincicau@yahoo.com>
2022-01-12 14:33:30 +0200

    File handling done on endpoint

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 from skimage import transform
+# from os.path import join, dirname, abspath
+from os import path, remove
 
 
 model = tf.keras.models.load_model('./model/my_model')
 
 app = Flask(__name__)
+app.config['IMAGE_UPLOADS'] = 'C:\\Users\Alex\\Desktop\\flask\\input'
 
 @app.route('/')
 def hello_world():
@@ -16,16 +19,16 @@ def hello_world():
 
 @app.route('/process', methods=['POST'])
 def process():
-    # imagefile = flask.request.files.get('imagefile', '')
-    # if 'file' in request.files:
-    #     print('yes')
-    print(request.files)
+    file = request.files['file']
+    file.save(path.join(app.config['IMAGE_UPLOADS'], 'file.jpg'))
+    file.close()
 
-    image = Image.open('./input/00001.jpg')
-    #image = request.files['file']
+    image = Image.open('./input/file.jpg')
     image = np.array(image).astype('float32')/255
     image = transform.resize(image, (120, 120, 3))
     image = np.expand_dims(image, axis=0)
+
+    remove('C:\\Users\Alex\\Desktop\\flask\\input\\file.jpg')
 
     result = model.predict(image)
     print(result)
